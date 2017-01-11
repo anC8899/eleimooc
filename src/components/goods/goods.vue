b632511758c3a9cd958d1031b734d2f360fa5823
anc8899 <shenzongwen@gmail.com>
2017-01-11 11:54:00 +0800

    编写 header 组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" v-el:menu-wrapper>
           <ul>
             <li v-for="item in goods" class="menu-item">
               <span class="text border-1px">
@@ -8,7 +8,30 @@
             </li>
           </ul>
         </div>
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper" v-el:foods-wrapper>
+          <ul>
+            <li v-for="item in goods" class="food-list">
+              <h1 class="title">{{item.name}}</h1>
+              <ul>
+                <li v-for="food in item.foods" class="food-item border-1px">
+                  <div class="icon">
+                    <img width="57" height="57" :src="food.icon">
+                  </div>
+                  <div class="content">
+                    <h2 class="name">{{food.name}}</h2>
+                    <p class="desc">{{food.description}}</p>
+                    <div class="extra">
+                      <span class="count">月售{{food.sellCount}}</span><span>好评率{{food.rating}}%</span>
+                    </div>
+                    <div class="price">
+                      <span class="now">￥{{food.price}}</span><span v-if="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
     </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus">
@@ -39,34 +62,94 @@
           vertical-align: middle
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
-
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background-color: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          width: 57px
+          height: 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            height: 14px
+            line-height: 14px
+            margin: 2px 0 8px 0
+            color: rgb(7, 17, 27)
+            font-size: 14px
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              font-weight: 700;
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
 <script type="text/ecmascript-6">
-    import icon from 'components/icon/icon';
-    const ERR_OK = 0;
-    export default{
-      props: {
-        seller: {
-          type: Object
-        }
-      },
-      data() {
-        return {
-          goods: []
-        };
-      },
-      created() {
-        this.$http.get('/api/goods').then((response) => {
-          response = response.body;
-          if (response.errno === ERR_OK) {
-            this.goods = response.data;
-          }
-        });
-      },
-      components: {
-        icon
+  import icon from 'components/icon/icon';
+  import BScroll from 'better-scroll';
+
+  const ERR_OK = 0;
+  export default{
+    props: {
+      seller: {
+        type: Object
       }
-    };
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+          });
+        }
+      });
+    },
+    components: {
+      icon
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+      }
+    }
+  };
 </script>
